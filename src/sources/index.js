77777b283a12be91e26156b9006779d72f1c64f6
Tony Holdstock-Brown <tony@docker.com>
2016-02-25 14:43:46 -0800

    Restructure sources to use internal definitions member

--- src/sources/index.js
+++ src/sources/index.js
@@ -1,48 +1,6 @@
 'use strict';
 
-let definitions = new Map();
-
-/**
- * @param sources
- * @param array  array of source definitions
- */
-const processDefinitions = (ctx, toProcess) => {
-  if ( ! Array.isArray(toProcess)) {
-    throw new Error('Source definitions must be defined in an array');
-  }
-
-  toProcess.forEach(def => {
-    validateDefinition(def);
-
-    // Get the fields and modelType from the source definition's return value
-    let [fields, modelType] = def.returns;
-
-    // Return all existing source definitions which return the given model
-    // type, eg. all sources which return a single user (User.item)
-    // TODO: Polymorphic API responses
-    let modelDefinitions = definitions.get(modelType) || [];
-
-    // Add the fields which are returned from this source definition to the
-    // object for easier manipulation and iteration later.
-    const definition = {
-      ...def,
-      fields
-    };
-
-    // Store this source definition naively.
-    //
-    // When looking for a suitable source definition we need to take into
-    // consideration what the source *returns* and its *parameters*.
-    //
-    // TODO: Store the definition based on the fields that it returns. We need
-    // an efficient way of iterating through all source definitions for a given
-    // return type to find the best source or combination of sources which
-    // return the requested data.
-    modelDefinitions.push(definitions);
-
-    definitions.set(modelType, modelDefinitions);
-  });
-}
+import Returns from './returns';
 
 /**
  * These keys are required in every source definition
@@ -78,19 +36,101 @@ const validateDefinition = (def) => {
  */
 export default class Sources {
 
+  _definitions = new Map()
+
   /**
    * @param object Object containing keys to driver functions
    */
   constructor(drivers) {
     this.drivers = drivers;
 
-    // Make each driver callable via its key bound to our current context
+    // Make each driver callable via its key bound to our current context. This
+    // allows us to add definitions for each driver by calling
+    // sources[driverName](), ie: sources.fromSDK([...]);
     Object.keys(drivers).forEach(key => {
-      this[key] = (...args) => {
-        drivers[key](this, ...args);
-        processDefinitions(this, ...args)
-      }
+      const driverFunc = drivers[key];
+      // When calling the driver name run processDefinitions to add the
+      // definitions to the Source class
+      this[key] = (defs) => this.processDefinitions(driverFunc, defs)
     });
+  }
+
+  /**
+   * @param sources
+   * @param array  array of source definitions
+   */
+  processDefinitions(driverFunc, defsToProcess) {
+    if ( ! Array.isArray(defsToProcess)) {
+      throw new Error('Source definitions must be defined in an array');
+    }
+
+    defsToProcess.forEach(def => {
+      // Ensure that the definition is valid. This will throw an error and
+      // prevent any future JS from being called.
+      validateDefinition(def);
+
+      if (def.returns instanceof Returns) {
+        // This is a non-polymorphic source which only returns one type of model.
+        // We can handle this individually.
+        return addDefinition(def);
+      }
+
+      // This may be a polymorphic source which returns more than one Model. If
+      // so, the object's keys should be the same as the source response to
+      // assign model attributes:
+      //
+      // {
+      //   user: User.item,
+      //   likes: Likes.list
+      // }
+      //
+      // This expects a response from the source to contain an object in the
+      // same structure:
+      // {
+      //   user: {...}, // user attributes
+      //   likes: [{...}, {...}], // likes list
+      // }
+      //
+      // We add each of these individually so we can map them by the return
+      // model.
+      if (typeof def.returns === 'object') {
+        Object.keys(def.returns).forEach(returnKey => {
+          addDefinition(def.returns[returnKey]);
+        });
+      }
+
+      throw new Error(
+        'Invalid return definition with type of ' +
+        typeof def.returns
+      );
+    });
+  }
+
+  addDefinition(def) {
+    // Return all existing source definitions which return the given model
+    // type, eg. all sources which return a single user (User.item)
+    // TODO: Polymorphic API responses
+    let modelDefinitions = this._definitions.get(modelType) || [];
+
+    // Add the fields which are returned from this source definition to the
+    // object for easier manipulation and iteration later.
+    const definition = {
+      ...def,
+      fields
+    };
+
+    // Store this source definition naively.
+    //
+    // When looking for a suitable source definition we need to take into
+    // consideration what the source *returns* and its *parameters*.
+    //
+    // TODO: Store the definition based on the fields that it returns. We need
+    // an efficient way of iterating through all source definitions for a given
+    // return type to find the best source or combination of sources which
+    // return the requested data.
+    modelDefinitions.push(definitions);
+
+    this._definitions.set(modelType, modelDefinitions);
   }
 
 }
--- test/sources/drivers.js
+++ test/sources/drivers.js
@@ -6,25 +6,14 @@ import Sources from '../../src/sources';
 describe('Defining source drivers', () => {
 
   it('adds methods to the source class', () => {
-    let called = false;
-
     const s = new Sources({
-      test: () => called = true
+      test: () => ({})
     });
-
     assert.isFunction(s.test);
-    s.test([]);
-    assert.isTrue(called);
   });
 
-  it('passes correct context arg when calling drivers', () => {
-    let ctx;
-    const s = new Sources({
-      test: (context) => ctx = context
-    });
-
-    s.test([]);
-    assert.equal(s, ctx);
+  xit('calls the driver method with then, fail, params and source', () => {
+    // TODO Ensure that drivers are called correctly when asked to load a model
   });
 
 });
