b6c8a013fa32da5e69c9be2de6237e79a601d236
Tony Holdstock-Brown <tony@docker.com>
2016-08-07 16:17:52 -0700

    Add tests for model extension

--- test/specs/model/extending.js
+++ test/specs/model/extending.js
@@ -1,14 +1,38 @@
 'use strict';
 
-import { User } from '/test/models';
+import { assert } from 'chai';
 
-class Admin extends User {
+import Model from '/src/model';
+
+class Foo extends Model {
+  static modelName = 'foo';
+
+  static fields = {
+    id: 0,
+    name: ''
+  };
+
   foo() {
+    return 'foo';
+  }
+}
+
+class Bar extends Foo {
+  bar() {
     return 'bar';
   }
 }
 
 describe('extending models', () => {
   it('inherits from parent class', () => {
+    const a = new Bar();
+    assert.deepEqual(a.constructor.fields, {id: 0, name: ''});
+    assert.deepEqual(a.constructor.fieldNames(), ['id', 'name']);
+    assert.equal(a.constructor.modelName, 'foo');
+  });
+  it('can call parent methods', () => {
+    const a = new Bar();
+    assert.equal(a.foo(), 'foo');
+    assert.equal(a.bar(), 'bar');
   });
 });
