180a70772e4d3c5f06ad2d1663e18bfdbaab6dd5
Tony Holdstock-Brown <tony@docker.com>
2016-09-21 12:06:19 -0700

    Fix typo

--- packages/tectonic/src/decorator/index.js
+++ packages/tectonic/src/decorator/index.js
@@ -119,7 +119,7 @@ export default function load(queries) {
         this.addAndResolveQueries();
       }
 
-      componnetWillUnmount() {
+      componetWillUnmount() {
         this.queries = {};
       }
 
