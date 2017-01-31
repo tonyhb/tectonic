3761e72c0e27a4da84ebd13609dc3004cdae4ccc
Tony Holdstock-Brown <tony@docker.com>
2017-01-30 16:22:03 -0800

    Add reducer tests

--- packages/tectonic/test/specs/reducer/reducer.js
+++ packages/tectonic/test/specs/reducer/reducer.js
@@ -1,13 +1,79 @@
 import { assert } from 'chai';
 
+import { Map } from 'immutable';
 import Model from '../../../src/model';
+import { User, Post } from '../../models';
+import reducer, {
+  UPDATE_DATA,
+  DELETE_DATA,
+} from '../../../src/reducer';
 
 describe('reducer', () => {
+
   describe('UPDATE_DATA', () => {
-    xit('updates data in `data` map', () => {
+    const now = new Date();
+    const query = User.getItem({ id: 1 });
+    query.returnedIds = [1];
+    const data = {
+      'users': {
+        '1': {
+          data: {
+            name: 'foo',
+          },
+          cache: {
+            expires: now,
+          },
+        },
+      },
+    };
+
+    it('updates data in `data` map', () => {
+      const state = reducer(undefined, {
+        type: UPDATE_DATA,
+        payload: {
+          data,
+          query,
+          expires: now,
+        },
+      });
+      assert.deepEqual(state.get('data').toJS(), data);
     });
 
-    xit('unsets any previously set deleted fields in fetched models', () => {
+    it('unsets any previously set deleted fields in fetched models', () => {
+      // First set data
+      let state = reducer(undefined, {
+        type: UPDATE_DATA,
+        payload: {
+          data,
+          query,
+          expires: now,
+        },
+      });
+      // Then delete it
+      state = reducer(state, {
+        type: DELETE_DATA,
+        payload: {
+          modelName: 'users',
+          modelId: '1',
+          query
+        },
+      });
+      assert.equal(state.getIn(['data', 'users', '1', 'deleted']), true);
+      console.log(state.get('data'));
+
+      // Then reset data ia UPDATE_DATA
+      state = reducer(undefined, {
+        type: UPDATE_DATA,
+        payload: {
+          data,
+          query,
+          expires: now,
+        },
+      });
+
+      assert.deepEqual(state.get('data').toJS(), data);
+      assert.equal(!!state.getIn(['data', 'users', '1', 'deleted']), false);
+      console.log(state.get('data'));
     });
   });
 
